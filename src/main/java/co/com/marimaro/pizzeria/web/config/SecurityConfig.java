1b2b37a2539038f5f4f0b226809405bde8d5f810
Marco Osorio <marco.osorio86@hotmail.com>
2025-08-19 09:51:41 -0500

    Se crea un usuario ADMIN en memoria

--- src/main/java/co/com/marimaro/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/co/com/marimaro/pizzeria/web/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +26,23 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build(); //
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                // La nueva contraseña debe estar encriptada, si se envía una cadena de texto
+                // genera error
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        // Teniendo este usuario Spring dejará de crear un usuario por defecto
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
