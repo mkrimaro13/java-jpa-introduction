ebe91c5b44863ce9ba092a5ded1a041c3cb96697
Marco Osorio <marco.osorio86@hotmail.com>
2025-08-20 16:05:55 -0500

    Se comenta el método que crea usuarios en memoria

--- src/main/java/co/com/marimaro/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/co/com/marimaro/pizzeria/web/config/SecurityConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,25 +31,25 @@ public class SecurityConfig {
         return httpSecurity.build(); //
     }
 
-    @Bean
-    public UserDetailsService memoryUsers() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                // La nueva contraseña debe estar encriptada, si se envía una cadena de texto
-                // genera error
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-        // Teniendo este usuario Spring dejará de crear un usuario por defecto
+    // @Bean
+    // public UserDetailsService memoryUsers() {
+    //     UserDetails admin = User.builder()
+    //             .username("admin")
+    //             // La nueva contraseña debe estar encriptada, si se envía una cadena de texto
+    //             // genera error
+    //             .password(passwordEncoder().encode("admin"))
+    //             .roles("ADMIN")
+    //             .build();
+    //     // Teniendo este usuario Spring dejará de crear un usuario por defecto
 
-        UserDetails customer = User.builder()
-                .username("customer")
-                .password(passwordEncoder().encode("customer"))
-                .roles("CUSTOMER")
-                .build();
+    //     UserDetails customer = User.builder()
+    //             .username("customer")
+    //             .password(passwordEncoder().encode("customer"))
+    //             .roles("CUSTOMER")
+    //             .build();
 
-        return new InMemoryUserDetailsManager(admin, customer);
-    }
+    //     return new InMemoryUserDetailsManager(admin, customer);
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
