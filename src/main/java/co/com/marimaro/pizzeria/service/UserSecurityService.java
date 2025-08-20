3d987d9b914d018b02994362d523d6c147de46cc
Marco Osorio <marco.osorio86@hotmail.com>
2025-08-20 16:05:29 -0500

    Se crea la clase servicio que gestionará los usuarios extraidos de la base de datos implementando la interfaz UserDetailsService

---
+++ src/main/java/co/com/marimaro/pizzeria/service/UserSecurityService.java
@@ -0,0 +1,32 @@
+package co.com.marimaro.pizzeria.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import co.com.marimaro.pizzeria.persistance.entity.UserEntity;
+import co.com.marimaro.pizzeria.persistance.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario " + username + "no ha sido encontrado"));
+
+        return User.builder()
+                .username(user.getUsername()).password(user.getPassword())
+                .roles("ADMIN")
+                .accountLocked(user.getLocked())
+                .disabled(user.getDisabled())
+                .build();
+    }
+
+}
