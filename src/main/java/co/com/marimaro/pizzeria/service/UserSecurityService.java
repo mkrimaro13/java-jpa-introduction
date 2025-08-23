f5eced2517eea53630c014941a7c669188d74dcd
Marco Osorio <marco.osorio86@hotmail.com>
2025-08-23 15:47:39 -0500

    Se agrega el uso de authorities

--- src/main/java/co/com/marimaro/pizzeria/service/UserSecurityService.java
+++ src/main/java/co/com/marimaro/pizzeria/service/UserSecurityService.java
@@ -1,6 +1,11 @@
 package co.com.marimaro.pizzeria.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,24 +19,43 @@ import co.com.marimaro.pizzeria.persistance.repository.UserRepository;
 @Service
 public class UserSecurityService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findById(username)
-                .orElseThrow(() -> new UsernameNotFoundException("El usuario " + username + "no ha sido encontrado"));
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                UserEntity user = userRepository.findById(username)
+                                .orElseThrow(() -> new UsernameNotFoundException(
+                                                "El usuario " + username + "no ha sido encontrado"));
 
-        String[] roles = user.getRoles()
-                .stream().map(UserRole::getRole)
-                .toArray(String[]::new);
+                String[] roles = user.getRoles()
+                                .stream().map(UserRole::getRole)
+                                .toArray(String[]::new);
 
-        return User.builder()
-                .username(user.getUsername()).password(user.getPassword())
-                .roles(roles)
-                .accountLocked(user.getLocked())
-                .disabled(user.getDisabled())
-                .build();
-    }
+                return User.builder()
+                                .username(user.getUsername()).password(user.getPassword())
+                                // .roles(roles)
+                                .authorities(grantedAuthorities(roles))
+                                .accountLocked(user.getLocked())
+                                .disabled(user.getDisabled())
+                                .build();
+        }
 
+        private String[] getAuthorities(String role) {
+                if ("ADMIN".equals(role) || "CUSTOMER".equals(role)) {
+                        return new String[] { "random_order" };
+                }
+                return new String[] {};
+        }
+
+        private List<GrantedAuthority> grantedAuthorities(String[] roles) {
+                List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+                for (String role : roles) {
+                        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+                        for (String authority : this.getAuthorities(role)) {
+                                authorities.add(new SimpleGrantedAuthority(authority));
+                        }
+                }
+                return authorities;
+        }
 }
--- src/main/java/co/com/marimaro/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/co/com/marimaro/pizzeria/web/config/SecurityConfig.java
@@ -22,9 +22,10 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(customizer -> customizer
-                        .requestMatchers(HttpMethod.GET, "/pizzas/**").hasAnyRole("ADMIN","CUSTOMER")
+                        .requestMatchers(HttpMethod.GET, "/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
                         .requestMatchers(HttpMethod.POST, "/pizzas/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/pizzas/**").hasRole("ADMIN")
+                        .requestMatchers("/orders/create/random").hasAuthority("random_order")
                         .requestMatchers("/orders/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
@@ -33,22 +34,22 @@ public class SecurityConfig {
 
     // @Bean
     // public UserDetailsService memoryUsers() {
-    //     UserDetails admin = User.builder()
-    //             .username("admin")
-    //             // La nueva contraseña debe estar encriptada, si se envía una cadena de texto
-    //             // genera error
-    //             .password(passwordEncoder().encode("admin"))
-    //             .roles("ADMIN")
-    //             .build();
-    //     // Teniendo este usuario Spring dejará de crear un usuario por defecto
+    // UserDetails admin = User.builder()
+    // .username("admin")
+    // // La nueva contraseña debe estar encriptada, si se envía una cadena de texto
+    // // genera error
+    // .password(passwordEncoder().encode("admin"))
+    // .roles("ADMIN")
+    // .build();
+    // // Teniendo este usuario Spring dejará de crear un usuario por defecto
 
-    //     UserDetails customer = User.builder()
-    //             .username("customer")
-    //             .password(passwordEncoder().encode("customer"))
-    //             .roles("CUSTOMER")
-    //             .build();
+    // UserDetails customer = User.builder()
+    // .username("customer")
+    // .password(passwordEncoder().encode("customer"))
+    // .roles("CUSTOMER")
+    // .build();
 
-    //     return new InMemoryUserDetailsManager(admin, customer);
+    // return new InMemoryUserDetailsManager(admin, customer);
     // }
 
     @Bean
