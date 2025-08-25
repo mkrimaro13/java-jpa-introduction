768cb8d075411d46f432e41852f0cb43801af572
Marco Osorio <marco.osorio86@hotmail.com>
2025-08-25 10:33:39 -0500

    Se agrega la dependencia de auth0 para utilizar el JWT, y se crea una clase con una implementanción básica del JWT

---
+++ src/main/java/co/com/marimaro/pizzeria/web/config/JwtUtils.java
@@ -0,0 +1,29 @@
+package co.com.marimaro.pizzeria.web.config;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Component
+public class JwtUtils {
+    private static String SECRET_KEY;
+    private static Algorithm ALGORITHM;
+
+    static {
+        SECRET_KEY = "m4r1m4r0_p1zz4";
+        ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+    }
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("marimaro-pizza")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
